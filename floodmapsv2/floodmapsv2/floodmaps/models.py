f8b02ab9cf69265f8341f3a19b5f564e2c313dab
VincentHussey <vincent.hussey@opw.ie>
2012-08-10 16:51:27 +0100

    List models

--- floodmapsv2/floodmapsv2/floodmaps/models.py
+++ floodmapsv2/floodmapsv2/floodmaps/models.py
@@ -1,3 +1,38 @@
-from django.db import models
+#from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+# Historic Floods
+# historic flood points
+
+
+# historic flood polygons
+
+
+# Indicative Floods
+# indicative flood polygons
+
+
+
+# Predictive Floods
+# predictive flood polygons
+
+
+
+
+
+# Media
+# photographs
+
+
+# scanned images
+
+
+# uploaded files
+
+
+# videos
+
+
+
