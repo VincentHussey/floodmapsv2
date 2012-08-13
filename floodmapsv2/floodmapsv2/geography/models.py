37a288d94e3b464f9b5e6375d93327341a6d159c
VincentHussey <vincent.hussey@opw.ie>
2012-08-13 15:59:24 +0100

    Added migration details for geometry columns

--- floodmapsv2/floodmapsv2/geography/models.py
+++ floodmapsv2/floodmapsv2/geography/models.py
@@ -1,6 +1,10 @@
 #from django.db import models
 from django.contrib.gis.db import models
 
+from south.modelsinspector import add_introspection_rules
+add_introspection_rules([], ["^django\.contrib\.gis"])
+
+
 # Create your models here.
 
 # Coast
