199794ffb5e05e8c45c6c6ce62dcf27bf09d31b5
VincentHussey <vincent.hussey@opw.ie>
2012-08-13 15:24:28 +0100

    Add titles of models for geography app

--- floodmapsv2/floodmapsv2/geography/models.py
+++ floodmapsv2/floodmapsv2/geography/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+#from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+
+# Coast
+
+
+
+# Province
+ 
+
+
+# County
+
+
+
+# Town
+
+
+
+# Townland
+
+
+
+# Coastal water
+
+
+
+# Catchments
+
+
+
+# Rivers
+
+
+
+# Lakes
+
+
+
+# Roads
+
