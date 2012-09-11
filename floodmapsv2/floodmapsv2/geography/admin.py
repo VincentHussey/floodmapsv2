45bc06cdba27ffac2e20af47f85f2e7bb5cc4026
Vincent Hussey <vincent.hussey@opw.ie>
2012-09-11 11:43:56 +0000

    Added admin.py for Geography.

---
+++ floodmapsv2/floodmapsv2/geography/admin.py
@@ -0,0 +1,14 @@
+from django.contrib.gis import admin
+from geography.models import *
+
+admin.site.register(Coast)
+admin.site.register(Province)
+admin.site.register(County)
+admin.site.register(DistrictElectoralDivision)
+admin.site.register(Town)
+admin.site.register(Townland)
+admin.site.register(CoastalWater)
+admin.site.register(Catchment)
+admin.site.register(Road)
+admin.site.register(Lake)
+admin.site.register(River)
