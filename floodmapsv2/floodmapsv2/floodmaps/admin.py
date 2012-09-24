697164493bd0b3fdb7655a7a7eaf7f73700a72d5
Vincent Hussey <vincent.hussey@opw.ie>
2012-09-24 15:10:58 +0100

    Change default admin parameters.

--- floodmapsv2/floodmapsv2/floodmaps/admin.py
+++ floodmapsv2/floodmapsv2/floodmaps/admin.py
@@ -1,6 +1,17 @@
 from django.contrib.gis import admin
 from floodmaps.models import *
 
+class MyGeoAdmin(admin.OSMGeoAdmin): # uses WebMercator
+	default_lon = -884000 #-8.5 #degrees W
+	default_lat = 7061500 # 52.5 #degrees N
+	default_zoom = 6
+	display_srid = True
+	max_width = 800
+	max_height = 600
+	# map_srid = 900913 # uses WebMercator
+	debug = True
+	# openlayers_url = 'http://openlayers.org/api/2.12/OpenLayers.js'
+
 admin.site.register(Glossary)
 admin.site.register(GlossaryCategory)
 admin.site.register(ReportType)
@@ -12,7 +23,7 @@ admin.site.register(QualityCode)
 admin.site.register(InterpretationStage)
 admin.site.register(ApprovalStatus)
 admin.site.register(Report)
-admin.site.register(HistoricFlood, admin.OSMGeoAdmin)
+admin.site.register(HistoricFlood, MyGeoAdmin) #admin.OSMGeoAdmin)
 admin.site.register(ReportInterpretation)
 admin.site.register(HistoricFloodInterpretation)
 admin.site.register(FloodAddress)
