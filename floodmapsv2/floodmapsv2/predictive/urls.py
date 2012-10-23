3c15fd8769354a4ee86a8a2a8b73fcf24ac5b621
Vincent Hussey <vincent.hussey@opw.ie>
2012-10-23 22:39:37 +0100

    Working on viewer & extents view.

--- floodmapsv2/floodmapsv2/predictive/urls.py
+++ floodmapsv2/floodmapsv2/predictive/urls.py
@@ -6,5 +6,6 @@ from django.conf import settings
 urlpatterns = patterns('',
     url(r'^$', ListView.as_view(model=UnitOfManagement, template_name="floods_dir_maps.html")),
     url(r'^apsr/(?P<pk>\d+)/$', DetailView.as_view(model=AreaOfPotentialSignificantRisk)),
+    url(r'^extent/(?P<pk>\d+)/$', DetailView.as_view(model=FloodExtent)),
 )
 
